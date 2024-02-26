ef2127d1a51637e64340569e8c0598254b6031bf
Reuben <75034784+XorV2@users.noreply.github.com>
2024-02-26 12:07:02 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+echo "Setting up yo shit"
+echo "This setup is made for ubuntu, if you're not on ubuntu it won't work for you"
+
+apt-get update -y && apt-get upgrade -y && apt-get update -y
+apt-get install python3 -y
+apt-get install python3-pip -y
+apt-get install ufw
+
+# These should be installed automatically when you install python3, but just in-case
+pip3 install socket
+pip3 install json
+pip3 install time
+pip3 install random
+pip3 install sys
+pip3 install requests
+pip3 install threading
+pip3 install datetime
+
+# This will allow your server to accept port on the default tcp port for Silly Source 1337
+iptables -t mangle -I PREROUTING -p tcp --dport 1337 -j ACCEPT
+ufw enable
+ufw allow 1337/tcp
+ufw reload
